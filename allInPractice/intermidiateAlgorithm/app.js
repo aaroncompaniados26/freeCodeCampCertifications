ee33dd86a310f53cac532c5fb1d55925f2baa570
RrChaos <rrchaos@tutanota.com>
2018-10-15 16:45:00 +0100

    FunctionalProgramming & Algorithm in JS

--- allInPractice/intermidiateAlgorithm/app.js
+++ allInPractice/intermidiateAlgorithm/app.js
@@ -44,3 +44,34 @@ console.log(spinalCase('This Is Spinal Tap'));
     // underscore character [_] is encountered
     // or is followed by an uppercase letter [(?=[A-Z])]
 //--------------------------------------------------------------
+// Translate the provided string to pig latin.
+// Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+// If a word begins with a vowel you just add "way" to the end.
+// Input strings are guaranteed to be English words in all lowercase.
+function translatePigLatin(str) {
+  let checker = str.charAt(0);
+  let checkerDue = str.charAt(0) + str.charAt(1);
+  let res = /[eaiou]/gi;
+  return (res.test(checker)) ? `${str}way` :
+          (!res.test(checkerDue)) ? `${str.slice(2)}${str.slice(0,2)}ay` : `${str.slice(1)}${checker}ay`;
+}
+console.log(translatePigLatin("glove"));
+//--------------------------------------------------------------------
+// Usernames are used everywhere on the internet. They are what give users a unique identity on their favorite sites.
+// You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+// 1) The only numbers in the username have to be at the end. There can be zero or more of them at the end.
+// 2) Username letters can be lowercase and uppercase.
+// 3) Usernames have to be at least two characters long. A two-letter username can only use alphabet letter characters.
+{
+  let username = "JackOfAllTrades";
+  let userCheck = /^[a-z]{2,}\d*$/i;
+  let result = userCheck.test(username);
+}
+//-------------------------------------------------------------------
+// Change the regex ohRegex to match only 3 to 6 letter h's in the word "Oh no".
+{
+  let ohStr = "Ohhh no";
+  let ohRegex = /oh{3,6}\sno/i;
+  let result = ohRegex.test(ohStr);
+}
+//---------------------------------------------------------------
