25612d693d71a6c0903befd96c874539bd1da960
RrChaos <rrchaos@tutanota.com>
2019-03-10 10:45:54 +0000

    +ES6 & ES7 Challenges

--- ES6challenges/app.js
+++ ES6challenges/app.js
@@ -1,17 +1,18 @@
 //                                        COMPARING OBJECTS
 //Write a JavaScript program to compare two objects to determine 
 //if the first one contains equivalent property values to the second one.
-const comparer = (obj1, obj2) =>  Object.keys(obj1).length === Object.keys(obj2).length;
+const comparer = (obj1, obj2) => Object.keys(obj1).length === Object.keys(obj2).length;
 
 
-console.log(comparer({age: 25, hair: 'long'}, 
-         {hair: 'long', beard: true}));
+console.log(comparer({ age: 25, hair: 'long' }, { hair: 'long', beard: true }));
 
 
 //----------------------------------------CONVERT NUMBERS INTO AN ARRAY OF VALUES
 
 //Write a JavaScript program to converts a specified number to an array of digits.
-const digitize = n => [...`${n}`].map(i => parseInt(i));
+const digitize = n => [...
+    `${n}`
+].map(i => parseInt(i));
 
 console.log(digitize(123));
 
@@ -20,43 +21,78 @@ console.log(digitize(123));
 
 //Write a JavaScript program to filter out the specified values from an specified array. 
 //Return the original array without the filtered values.
-const filtering = (arr, [...items]) => { 
-      return arr.filter( current => { 
-            return !items.includes(current); 
-      });
-}; 
-                        
+const filtering = (arr, [...items]) => {
+    return arr.filter(current => {
+        return !items.includes(current);
+    });
+};
+
 console.log(filtering([1, 2, 3, 4, 5, 6, 7, 8], [1, 5]));
 
 
 //---------------------------------------EXTRACT FROM AN ARR THROUGH GIVEN INDEXES
+
 //Extract out the values at the specified indexes from an specified array
 const extractor = (arr, [...indexes]) => {
-      return indexes.map(index => arr[index]);
+    return indexes.map(index => arr[index]);
 }
 console.log(extractor([1, 2, 3, 4, 5, 6, 7, 8], [1, 5]));
 
 
 //----------------------------------------LENGTH CONVERTER INTO BYTES
 
-const byte_Size = str => new Blob([str]).size;  //byte measure
+const byte_Size = str => new Blob([str]).size; //byte measure
 
 console.log(byte_Size('123456'));
 
 
 //---------------------------------------TRUE if provided fn(all elements)
+
 //Write a JavaScript function that returns true if the provided predicate function returns true 
 //for all elements in a collection, false otherwise.
 const tru = (arr, fn = Boolean) => arr.every(fn);
 
-console.log( tru([4, 2, 3], x => x > 1) );
+console.log(tru([4, 2, 3], x => x > 1));
 
 
-//----------------------------------------
-//17
+//---------------------------------------- REMOVE FROM ARR (left)
+
+//JavaScript program to remove specified elements from the left of a given array of elements.
+const leftR = (arr, index = 1) => arr.slice(index);
+
+console.log(leftR([1, 2, 3], 2));
 
 
+//----------------------------------------- REMOVE FROM ARR (right)
+
+//Remove specified elements from the right of a given array of elements
+const rightR = (arr, index = -1) => arr.slice(`${- index}`);
+
+console.log(rightR([1, 2, 3], 2));
 
 
+//----------------------------------------- EXTEND A 3-digit Color Code
 
-//-----------------------------------------
+const digitExtend = code => {
+    return code.slice(code.startsWith('#') ? 1 : 0).split('').map(x => x + x).join('');
+}
+
+console.log(digitExtend('#03f'));
+console.log(digitExtend('03f'));
+
+//----------------------------------------- FILTER NON-UNIQUE values in an ARRAY
+
+const unique = arr => arr.filter(i => arr.indexOf(i) === arr.lastIndexOf(i));
+
+console.log(unique([1, 2, 2, 3, 4, 4, 5]));
+
+//---------------------------------------- DECAPITALIZE 1st letter of a string
+
+const decapitalize = ([first, ...rest]) => first.toLowerCase() + rest.join('');
+
+console.log(decapitalize('W3resource'));
+
+//----------------------------------------  
+//25
+
+
